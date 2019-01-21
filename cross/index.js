f0854f3a7c1f6bbc62e4ee65f84243172b3341e2
David <kioltk@gmail.com>
2019-01-21 21:10:15 +0200

    Added node.js bonjour version

---
+++ cross/index.js
@@ -0,0 +1,42 @@
+var bonjour = require("bonjour")();
+
+bonjour.publish({
+  name: "Bagel test",
+  type: "_Bagel._tcp",
+  domain: "",
+  port: 43435
+});
+
+// browse for all http services
+bonjour.find({ type: "_Bagel._tcp" }, function(service) {
+  console.log("Found an HTTP server:", service);
+});
+
+var net = require("net");
+
+var HOST = "192.168.1.19";
+var PORT = 43435;
+
+// Create a server instance, and chain the listen function to it
+// The function passed to net.createServer() becomes the event handler for the 'connection' event
+// The sock object the callback function receives UNIQUE for each connection
+net
+  .createServer(function(sock) {
+    // We have a connection - a socket object is assigned to the connection automatically
+    console.log("CONNECTED: " + sock.remoteAddress + ":" + sock.remotePort);
+
+    // Add a 'data' event handler to this instance of socket
+    sock.on("data", function(data) {
+      console.log("DATA " + sock.remoteAddress + ": " + data);
+      // Write the data back to the socket, the client will receive it as data from the server
+      sock.write('You said "' + data + '"');
+    });
+
+    // Add a 'close' event handler to this instance of socket
+    sock.on("close", function(data) {
+      console.log("CLOSED: " + sock.remoteAddress + " " + sock.remotePort);
+    });
+  })
+  .listen(PORT, HOST);
+
+console.log("Server listening on " + HOST + ":" + PORT);
